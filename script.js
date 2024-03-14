8bf15bec36441c77d0e79e96da5b88ecd1bc5d82
Edu Cuadrado <ecuadradoruiz@gmail.com>
2024-03-14 23:30:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,24 +3,34 @@
     ASÍ SIGUEN PUTEADOS UNOS CUANTOS DÍAS MÁS :)
 */
 const secretNumber = '585';
-let attempts = 0;
 
 function checkGuess() {
     const userGuessInput = document.getElementById('userGuess');
     const messageElement = document.getElementById('message');
-    const attemptCountElement = document.getElementById('attemptCount');
+    const shareButton = document.getElementById('shareButton');
 
     const userGuess = userGuessInput.value;
 
-        if (userGuess === secretNumber) {
-            messageElement.textContent = `¡ENHORABUENA CHICOS! 
-            POR FAVOR, PONED POR EL GRUPO EL NÚMERO Y PROCEDEREMOS CON LA RECOMPENSA :)`;
-            messageElement.style.color = 'green';
-            messageElement.style.margin = '10px';
-            userGuessInput.disabled = true;
-        } else {
-            messageElement.textContent = 'UPS! SEGUID PROBANDO...';
-            messageElement.style.color = 'red';
-            messageElement.style.margin = '10px';
-        }
+    if (userGuess === secretNumber) {
+        messageElement.textContent = `¡ENHORABUENA CHICOS! 
+        POR FAVOR, PONED POR EL GRUPO EL NÚMERO Y PROCEDEREMOS CON LA RECOMPENSA :)`;
+        messageElement.style.color = 'green';
+        messageElement.style.margin = '10px';
+        userGuessInput.disabled = true;
+        document.getElementById('checkButton').style.display = 'none'; // Oculta el botón de comprobar
+        shareButton.style.display = 'block'; // Muestra el botón de compartir
+    } else {
+        messageElement.textContent = 'UPS! SEGUID PROBANDO...';
+        messageElement.style.color = 'red';
+        messageElement.style.margin = '10px';
     }
+
+    // Desplazar el foco al input después de comprobar el intento
+    userGuessInput.focus();
+}
+
+function shareOnWhatsApp() {
+    const message = encodeURIComponent('¡Lo hemos adivinado! Es el ' + secretNumber + '.¡Queremos la recompensa!'); // Codifica el mensaje
+    const url = `https://wa.me/?text=${message}`; // Crea la URL con el mensaje
+    window.open(url, '_blank'); // Abre la URL en una nueva ventana
+}
